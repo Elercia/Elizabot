4dedbb670705e57723a2c5612e3bf5ce99ca49ef
Vincent RABIER <E155399M@U-INF-J-E114-08.iut-nantes.univ-nantes.prive>
2016-03-18 15:37:48 +0100

    Ajout des objets reponses et ElizaBot reponse definit ce que doit etre une reponse Elizabot est la classe principale

--- ElizaBot/questionner.js
+++ ElizaBot/questionner.js
@@ -3,11 +3,15 @@
 *le resultat correspond à la question à poser à l'utilisateur 
 *pour savoir si il connais les definitions correspondante au dépendance des mots clée rentré
 *
-*@return la question a poser à l'utilisateur  
+*@return la question a poser à l'utilisateur
+*on recupere les dépendance des mots clé utilisé puis on génére une reponse par rapport
 */
 function questionner_user(mot_clef)
 {
 	var resultat = "";
+	var mc = mot_clef[0];
+
+	
 
 	return resultat;  
 }
