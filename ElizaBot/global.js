bd99a9eb1b5a5d480f64d4957871d68912a86a87
Vincent <rabierv@hotmail.fr>
2016-03-18 13:17:27 +0100

    Ajout exemple class reponse à utiliser pour programme

--- ElizaBot/global.js
+++ ElizaBot/global.js
@@ -1,1 +1,18 @@
-//fichier qui donnne exemple
+//fichier des fonctions et des veriables globale qui seront utilisée par tous le monde
+
+function reponse(mots_cle, ensemble_def, ensemble_dependance)
+{
+	if(mots_cle === "undefined" && ensemble_dependance === "undefined" && ensemble_def === "undefined")
+	{
+		alert("Movaise rentrée de définition");
+	}
+	this.mots_cle = mots_cle;
+	this.ensemble_def = ensemble_def;
+	this.ensemble_dependance = ensemble_dependance;
+}
+
+reponse.prototype.methode1 = function() {
+	//corps de la methode
+};
+
+var a = new reponse({}, {}, {});//déclaration objet reponse
--- ElizaBot/questionner.js
+++ ElizaBot/questionner.js
@@ -1,4 +1,13 @@
-function questionner_user()
+
+/**
+*le resultat correspond à la question à poser à l'utilisateur 
+*pour savoir si il connais les definitions correspondante au dépendance des mots clée rentré
+*
+*@return la question a poser à l'utilisateur  
+*/
+function questionner_user(mot_clef)
 {
-	
+	var resultat = "";
+
+	return resultat;  
 }
