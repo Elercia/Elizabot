b995f58caea88bb34e1e1a73cae707cf7b382112
Vincent RABIER <E155399M@U-INF-J-F113-01.iut-nantes.univ-nantes.prive>
2016-03-21 12:57:32 +0100

    Commit ajout fonctio questionner_user

--- ElizaBot/questionner.js
+++ ElizaBot/questionner.js
@@ -4,13 +4,19 @@
  * en gérant la dépendance des connaissances
  *
  * @method     questionner_user
- * @param      {Array}  mot_clef  { L'ensemble des mots clés correspondant à la question à poser }
- * @return     {string}  { Le string correspondant à la question à poser }
+ * @param      {Array}  mot_clef  L'ensemble des mots clés correspondant à la question à poser 
+ * @return     {string}  Le string correspondant à la question à poser 
  */
 ElizaBot.prototype.questionner_user = function(mot_clef)
 {
-	var resultat = "";
-	var mc = mot_clef[0];
+	var resultat = "Connaît tu la notion de ";
+
+	var rep = this.rechercher_correspondance(mot_clef[0]);//on recherche la reponse correspondnat au mot clé que l'on nous a donné
+	var dep = rep.ensemble_dependance;//on recupere ces dépendances
+
+	rep = this.rechercher_correspondance(dep[0]);//on recherche une reponse par rapport aux dépendances.
+
+	resultat+= rep.mots_cle[0];
 
 	return resultat;  
 }
