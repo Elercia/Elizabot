d389820a5dd16146c54832a4a0acf8347a63f87f
Lucas Martineau <lucasmartineau@yahoo.fr>
2016-03-21 12:04:33 +0100

    amélioration donner_reponse

--- ElizaBot/reponse_user.js
+++ ElizaBot/reponse_user.js
@@ -1,7 +1,9 @@
 //																																													
-function donner_reponse()
+ElizaBot.prototype.donner_reponse = function()
 {
 	var saisie_utilisateur = document.getElementById("user_input_text").value;
-	var mot_clef = [];
-	mot_clef = rechercher_mot_cle(saisie_utilisateur);
+	var mot_clef = rechercher_mot_cle(saisie_utilisateur);
+	laReponse = ElizaBot.prototype.rechercher_correspondance(mot_clef);
+	var reponse = laReponse.ensemble_def;
+	ElizaBot.prototype.afficher_reponse(reponse);
 }
