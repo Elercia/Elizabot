6a494f608313b0ae3dcc279b94b05f468bc41e44
Lucas Martineau <lucasmartineau@yahoo.fr>
2016-03-23 09:21:15 +0100

    Ajout du 'je ne comprend pas'

--- ElizaBot/reponse_user.js
+++ ElizaBot/reponse_user.js
@@ -3,12 +3,16 @@ ElizaBot.prototype.donner_reponse = function()
 {
 	if (document.getElementById("user_input_text").value != "")
 	{
-		document.getElementById("historique").value += "\n[USER]  : " + document.getElementById("user_input_text").value;
-		document.getElementById("user_input_text").value = "";
 		var saisie_utilisateur = document.getElementById("user_input_text").value;
 		var mot_clef = rechercher_mot_cle(saisie_utilisateur);
-		laReponse = ElizaBot.prototype.rechercher_correspondance(mot_clef);
-		var reponse = laReponse.ensemble_def;
-		ElizaBot.prototype.afficher_reponse(reponse);
+		var laReponse = this.rechercher_correspondance(mot_clef);
+		if (typeof reponse!="undefined") {
+			var reponse = laReponse.ensemble_def;
+			this.afficher_reponse(reponse);
+		}
+		else
+		{
+			this.afficher_reponse("Je ne comprend pas");
+		}
 	}
 }
