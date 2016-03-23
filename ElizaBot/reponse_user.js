630681866617f8145df41af6ef9ad370cf44463a
Lucas Martineau <lucasmartineau@yahoo.fr>
2016-03-23 14:17:18 +0100

    appel à questionner()

--- ElizaBot/reponse_user.js
+++ ElizaBot/reponse_user.js
@@ -1,14 +1,23 @@
-//																																													
+/**
+*Fonction qui permet de donner des réponse lorsque l'utilisateur envoie son message
+*/											
 ElizaBot.prototype.donner_reponse = function()
 {
 	if (document.getElementById("user_input_text").value != "")
 	{
 		var saisie_utilisateur = document.getElementById("user_input_text").value;
 		var mot_clef = rechercher_mot_cle(saisie_utilisateur);
-		var laReponse = this.rechercher_correspondance(mot_clef);
-		if (typeof laReponse!="undefined") {
-			var reponse = laReponse.ensemble_def;
-			this.afficher_reponse(reponse);
+		var reponse = this.rechercher_correspondance(mot_clef);
+		if (typeof reponse!="undefined") {
+			if (reponse.ensemble_def == "incomprehension") //
+			{
+				var affichage = questionner(mot_clef);
+			}
+			else
+			{
+				var affichage = reponse.ensemble_def;
+			}
+			this.afficher_reponse(affichage);
 		}
 		else
 		{
