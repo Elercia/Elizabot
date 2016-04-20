4ec27c8e18810bd22dc33233cef1e5f6552b70a3
Vincent Rabier <rabierv@hotmail.fr>
2016-04-20 10:38:46 +0200

    mise a jour

--- ElizaBot/questionner.js
+++ ElizaBot/questionner.js
@@ -3,27 +3,29 @@
  * en gérant la dépendance des connaissances
  *
  * @method     questionner_user
- * @param      {Array}  mot_clef  L'ensemble des mots clés correspondant à la question à poser 
+ * @param      {String}  mot_clef  L'ensemble des mots clés correspondant à la question à poser 
  * @return     {string}  Le string correspondant à la question à poser 
  */
 ElizaBot.prototype.questionner_user = function(mot_clef)
 {
-	var resultat = "Connaît tu la notion de ";
+	var resultat = "Tu ne comprend pas la notion de ";
 
 	var rep1 = this.rechercher_correspondance(mot_clef);//on recherche la reponse correspondant au mot clé que l'on nous a donné
 
 	if(!(rep1 instanceof Reponse))
 	{
-		resultat = "je n'ai malheureusement pas de question à te poser";
+		resultat = "je ne peu malheureusement pas t'aider";
 	}
 	else
 	{
 		var dep = rep1.ensemble_dependance;//on récupère ces dépendances
-		if(typeof dep[0] === "Array")
-			resultat = dep[0].join(" ");
+		
+		if(Array.isArray(dep))
+			resultat += dep.join(" ou de ");
 		else
-			resultat += dep.join(" ");
+			resultat += dep;
 	}
+	resultat += "?";
 	/* TODO : modifier "this.dernier_mot_clef" pour que ça coïncide
 	(Que ça ait un rapport avec ce qu'on vient de lui demander)*/
 	this.dernier_mot_clef;
