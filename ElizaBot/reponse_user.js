bbd898ed85ec78adac777cffa2120ab9b1e35b76
Lucas Martineau <lucasmartineau@yahoo.fr>
2016-04-21 12:09:16 +0200

    incompréhension

--- ElizaBot/reponse_user.js
+++ ElizaBot/reponse_user.js
@@ -3,25 +3,25 @@
 */											
 ElizaBot.prototype.donner_reponse = function()
 {
+	//récupere la saisie de l'utilisateur
 	var saisie_utilisateur = document.getElementById("user_input_text").value;
 
+	//Si la saisie ne ressemble pas à une commande
 	if (saisie_utilisateur[0] != "/")
 	{
+		//S'il y a bien quelque chose d'écrit
 		if (saisie_utilisateur != "")
 		{
-			//récupere la saisie de l'utilisateur
-			
 			this.derniers_messages.push(saisie_utilisateur);
 
 			//recherche le ou les mots clefs faisant partie de la BDD dans la phrase saisie
 			var mot_cle = this.recherche_mot_cle(saisie_utilisateur);
-
 			//Si les nouveaux mots cles sont identiques à ceux de la demande précédente 
 			if ((saisie_utilisateur == this.derniers_messages[this.derniers_messages.length-2]) && (this.derniers_messages.length!=0))
 			{
 				mot_cle = ["sameinput"];
 			}
-
+			
 			//Traitement des mots reçus pour n'avoir que les mots cles les plus long ex : "graphe/ graphe connexe"
 			for (i in mot_cle)
 			{
@@ -34,46 +34,79 @@ ElizaBot.prototype.donner_reponse = function()
 				}
 			}
 
+			var incomprehension = false;
+			for (i in mot_cle)
+			{
+				var correspondance = this.rechercher_correspondance(mot_cle[i]);
+				if (String(correspondance.ensemble_def)=="incomprehension")
+				{
+					incomprehension = true;
+					console.log("incomprehension=true");
+				}
+
+			}
+
 			//S'il y a plus d'un mot clé, on demandera à l'utilisateur de quel notion il veut parler
-			if (mot_cle.length>1)
+			if ((mot_cle.length>1) && (incomprehension==false))
 			{
 				this.choisir_mot_cle(mot_cle);
 			}
 			else //Sinon on trouve la réponse pour lui envoyer
 			{
+				var affichage;
 				//on recherche la reponse (motclef/ensembledef/ensembledependance) correspondant au mot clef
-				var reponse = this.rechercher_correspondance(mot_cle);
-				
-				if (typeof reponse!="undefined")	//si la reponse existe
+				if (incomprehension == true)
 				{
-
-					/*Si le mot clef fait parti du champ lexical de l'incompréhension et a 
-					donc pour definition "incompréhension" */
-					if (reponse.ensemble_def == "incomprehension")
+					if (mot_cle.length>1)
 					{
-						//S'il n'a pas compris la notion, on va lui demander s'il connait les dépendances des notions 
-						var affichage = this.questionner_user(this.derniers_mots_cles);
-					}
-					else if (reponse.ensemble_def == "exemple")
-					{
-						//s'il demande un exemple, on lui donne un exemple du dernier mot clef utilisé
-						var affichage = this.donner_exemple(this.derniers_mots_cles);
+						affichage = this.questionner_user(mot_cle);
 					}
 					else
+					{
+						affichage = this.questionner_user(this.derniers_mots_cles[this.derniers_mots_cles.length-1]);
+					}
+					this.afficher_reponse(affichage);
+				}
+				else 
+				{
+					var reponse = this.rechercher_correspondance(mot_cle[0]);
+					console.log(reponse);
+					if (typeof reponse!="undefined")
 					{
 						if (mot_cle != "sameinput")
 						{
 							this.derniers_mots_cles.push(mot_cle);
 						}
-						var affichage = reponse.ensemble_def;
+						else if (reponse.ensemble_def == "exemple")
+						{
+							//s'il demande un exemple, on lui donne un exemple du dernier mot clef utilisé
+							affichage = this.donner_exemple(this.derniers_mots_cles);
+						}
+						affichage = reponse.ensemble_def;
+						this.afficher_reponse(affichage);
 					}
-					this.afficher_reponse(affichage);
+					else
+					{
+						this.pasTrouve();
+					}
+				}
+				//si la reponse existe
+	/*				
+				
+					
+					//si le mot clef fait parti du champ lexical de l'incompréhension et a 
+					//donc pour definition "incompréhension" 
 
-				}
-				else
-				{
-					this.pasTrouve();
-				}
+						//S'il y a plus d'un mot cle, et donc que l'utilisateur demande un notion en particulier
+
+					else
+					{
+						
+					}
+					
+
+				*/
+			
 			}
 			
 		}
@@ -84,7 +117,6 @@ ElizaBot.prototype.donner_reponse = function()
 		{
 			admin(true);
 			this.afficher("# INFO # L'interface administrateur a bien été dévoilée");
-			console.log("sorti");
 
 		}
 		else if (saisie_utilisateur.endsWith("adminoff"))
