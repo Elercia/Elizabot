4738c69a180badfd3cde1d759f68825b1a97be2d
lucasmart <lucasmartineau@yahoo.fr>
2016-05-14 22:38:51 +0200

    Update global.js
    ortographe.20100.remove

--- ElizaBot/js/global.js
+++ ElizaBot/js/global.js
@@ -187,12 +187,13 @@ new Reponse(["pute","conne","salope","petasse","con","putain","salop"], "Pas la 
     
     
 /**
- * .Fonction chargé au démarrage de la page Permmettant d'initialiser le nom de l'utilisateur 
+ * Cette fonction, chargée au démarrage de la page :
+ * -Initialise le nom de l'utilisateur 
  * (en fonction du localStorage)
- * 
- * .Génere un eventListener pour l'entrer d'un fichier dans la page HTML
- * 
- * .Initialise la conversation
+ * \n
+ * -Génere un eventListener pour l'entrée d'un fichier dans la page HTML
+ * \n
+ * -Initialise la conversation
  *
  * @method     chargement_page
  */ 
@@ -261,10 +262,10 @@ function chargement_page(){
 
 
 /**
- * Affiche ou non le boutton des définition
+ * Affiche ou non le bouton des définitions
  *
  * @method     admin
- * @param      {Boolean}  bool    { afficher ou non le menu déroulant pour la maintenance de la bdd}
+ * @param      {Boolean}  bool    { Si True : afficher - Si false : masquer}
  */
 function admin(bool)
 {
@@ -274,12 +275,13 @@ function admin(bool)
 
 
 /**
- * Ne pas ce fier au nom
- * Elle utilise le LocalStorage pour stocker des information tel que la base de donné ou le nom de l'utilisateur
+ * Ne pas se fier au nom
+ * \n
+ * La fonction utilise le LocalStorage pour stocker des informations, telles que la base de données ou le nom de l'utilisateur
  *
  * @method     createCookie
- * @param      {String}  name    { le nom de la donné stockée }
- * @param      {String}  value   { la valeur de la donné stockée }
+ * @param      {String}  name    { le nom de la donnée stockée }
+ * @param      {String}  value   { la valeur de la donnée stockée }
  * @param      {number}  days    { parameter_description }
  */
 function createCookie(name,value,days) {
@@ -294,7 +296,8 @@ function createCookie(name,value,days) {
 }
 
 /**
- * Permet de récupérer la valeur d'une donné local stocké
+ * Comme son nom ne l'indique pas,
+ * Permet de récupérer la valeur d'une donnée local stockée
  *
  * @method     readCookie
  * @param      {string}  name    { le nom de la donné local }
@@ -317,7 +320,7 @@ function readCookie(name) {
 }
 
 /**
- * Supprime une donné local
+ * Supprime une donnée en local
  *
  * @method     eraseCookie
  * @param      {<type>}  name    { parameter_description }
@@ -327,7 +330,7 @@ function eraseCookie(name) {
 }
 
 /**
- * Permet de modifier le nom de l'utilisateur à tous moment dans le pogramme
+ * Permet de modifier le nom de l'utilisateur à tout moment dans le pogramme
  *
  * @method     modifier_nom
  * @param      {(Function|string)}  value   { parameter_description }
